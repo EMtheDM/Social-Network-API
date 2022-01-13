117deed49f54fe0351b843481ac2e86045e95da6
Eric Martin <eric_martin@me.com>
2022-01-12 20:05:19 -0700

    created config, index and models

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+// const userSchema = require('./User');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+            unique: true,
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought",
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+const User = model('user', userSchema);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+module.exports = User;
