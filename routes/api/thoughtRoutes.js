0db1e5625158eeb19bf9eed06bd9179d9a67c060
Eric Martin <eric_martin@me.com>
2022-01-12 20:29:35 -0700

    created routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thoughtController');
+const { route } = require('./userRoutes');
+
+// /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts);
+
+// /api/thoughts/:thoughtId
+router
+    .route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought);
+
+// /api/thoughts/:userId
+router
+    .route('/:userId')
+    .post(createThought);
+
+// /api/thoughts/:userId/:thoughtId
+router
+    .route('/:userId/:thoughtId')
+    .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(removeReaction);
+
+module.exports = router;
